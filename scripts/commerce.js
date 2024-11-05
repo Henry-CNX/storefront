688125d7ce195678a9462a9b985f46adaf69380d
echavez <echavez@pkglobal.com>
2024-11-04 19:05:08 -0600

    Add more headers values

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -101,7 +101,7 @@ ${priceFieldsFragment}`;
 
 export async function performCatalogServiceQuery(query, variables) {
   const headers = {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/graphql',
     'Magento-Environment-Id': await getConfigValue('commerce-environment-id'),
     'Magento-Website-Code': await getConfigValue('commerce-website-code'),
     'Magento-Store-View-Code': await getConfigValue('commerce-store-view-code'),
